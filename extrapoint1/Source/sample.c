98392699c9307cb50180cc7dae35af508b711c29
Andrei <andrei.stefand@outlook.com>
2026-01-01 13:13:58 +0100

    feat: random tetromino gen on RESET

--- extrapoint1/Source/sample.c
+++ extrapoint1/Source/sample.c
@@ -13,33 +13,30 @@ extern uint8_t ScaleFlag; // <- ScaleFlag needs to visible in order for the emul
 #endif
 
 extern volatile Tetromino_t falling_tetromino;
+extern volatile bool game_paused;
 
 int main(void) {
     SystemInit(); /* System Initialization (i.e., PLL)  */
     LCD_Initialization();
     BUTTON_init();
     joystick_init();
-    // init_RIT(5000000); // 50ms
     init_RIT(250500);
     enable_RIT();
 
     init_timer(0, 0, 0, 3, 2500000);
     enable_timer(0);
-    srand(LPC_TIM0->TC);
-    disable_timer(0);
-
-
     init_timer(1, 0, 0, 3, 250500);
-
+    
     LCD_Clear(Black);
-
     update_leaderboard();
     init_game_field();
-
     print_or_delete_paused_text();
-
-    falling_tetromino = generate_tetromino();
+    
     enable_timer(1);
+    while(game_paused);
+    srand(LPC_TIM0->TC);
+    disable_timer(0);
+    falling_tetromino = generate_tetromino();
 
     LPC_SC->PCON |= 0x1; /* power-down    mode */
     LPC_SC->PCON &= ~(0x2);
